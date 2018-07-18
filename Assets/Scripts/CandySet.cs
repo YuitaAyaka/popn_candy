3a32514d035dcab02a3998c13a5ed441985f4d26
YuitaAyaka <53k76e9tay@gmail.com>
2018-07-18 22:39:38 +0900

    キャンディの引き継ぎ

---
+++ Assets/Scripts/CandySet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandySet : MonoBehaviour {
+
+		public GameObject[] candys;
+
+		// Use this for initialization
+		void Start () {
+		for (int i = 0; i < candys.Length; i++) {
+			if (i < GlobalParameters.candy_num) {
+				candys [i].SetActive (true);
+				}else{
+				candys [i].SetActive (false);
+				}
+			}
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+	}
