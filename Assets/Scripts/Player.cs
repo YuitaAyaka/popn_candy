0ce2f00f5d00ed182d341d678f7b53b3ce0aeba8
YuitaAyaka <53k76e9tay@gmail.com>
2018-08-27 12:24:48 +0900

    2-2,2-3,2のボーナスステージ
    2-2,2-3,ボーナスステージ追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -201,6 +201,10 @@ public class Player : MonoBehaviour {
 				SceneNavigator.Instance.Change("course1-2", 0.5f);
 			} else if (collision.gameObject.name == "STAR") {
 				SceneNavigator.Instance.Change("course1_Gaul", 0.5f);
+			} else if (collision.gameObject.name == "warp7") {
+				SceneNavigator.Instance.Change("course2-2", 0.5f);
+			} else if (collision.gameObject.name == "warp6") {
+				SceneNavigator.Instance.Change("course2-1bonus_stage", 0.5f);
 			}
 
 
