977439a800929625eaa05c4b00431c37a5c9fc40
YuitaAyaka <53k76e9tay@gmail.com>
2018-09-28 21:28:47 +0900

    更新
    更新

---
+++ Assets/Scripts/DontShowScreenReset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public class DontShowScreenReset : MonoBehaviour {
+	public float speed = 10;
+	public int spriteCount = 3;
+	void Update () {
+		// 左へ移動
+		transform.position += Vector3.left * speed * Time.deltaTime;
+	}
+	void OnBecameInvisible()
+	{
+		// スプライトの幅を取得
+		float width = GetComponent<SpriteRenderer>().bounds.size.x;
+		// 幅ｘ個数分だけ右へ移動
+		transform.position += Vector3.right * width * spriteCount;
+	}
+}
---
+++ Assets/Scripts/DontShowScreenReset_tree.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+public class DontShowScreenReset_tree : MonoBehaviour {
+	//public float moveSpeed;
+	public float speed = 10;
+	public int spriteCount = 3;
+	void Update () {
+		
+		// 左へ移動
+	
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+
+		// 右へ移動
+
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+	}
+
+	void OnBecameInvisible()
+	{
+		// スプライトの幅を取得
+		float width = GetComponent<SpriteRenderer>().bounds.size.x;
+		// 幅ｘ個数分だけ右へ移動
+		transform.position += Vector3.right * width * spriteCount;
+	}
+}
+
+
