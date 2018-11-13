0d6abfa5443ea4ba827fe0fb4c8ab950e9b4b668
YuitaAyaka <53k76e9tay@gmail.com>
2018-11-13 23:29:17 +0900

    いらないものを消しました

---
+++ Assets/Scripts/ChangeColorScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColorScript : MonoBehaviour {
+
+	//public GameObject myCube;
+
+	// Use this for initialization
+	void Start () 
+	{	
+		Color rand = new Color(Random.value, Random.value, Random.value, 1.0f);
+		//renderer.material.color = rand;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
