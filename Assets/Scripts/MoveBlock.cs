339d39a8cf421a9d14eaf96c29e6cba7c02b7a66
YuitaAyaka <53k76e9tay@gmail.com>
2018-11-18 21:43:20 +0900

    クレジットの追加

---
+++ Assets/Scripts/MoveBlock.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBlock : MonoBehaviour
+{
+
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+
+        initialPosition = transform.position;
+
+    }
+
+    void Update()
+    {
+
+        transform.position = new Vector3(initialPosition.x, Mathf.Sin(Time.time) * 14.0f + initialPosition.y, initialPosition.z);
+
+    }
+}
