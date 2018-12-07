26e3d8b98b673170ee81c7854694a53d259732dc
YuitaAyaka <53k76e9tay@gmail.com>
2018-12-07 11:59:58 +0900

    1分間のやつ
    1分間画面に触らない、またはクリックしないとシーンが飛ぶスクリプト。スタート画面に飛ぶようにしておくといい感じです。
    展示に最適です。

---
+++ Assets/Scripts/OneMinute.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneMinute : MonoBehaviour
+{
+
+    public float timeLimit = 60.0f;
+    public float sleepTime = 0.0f;
+    private bool flagonetime = false;
+    public string SceneName;
+
+
+    public void Update()
+    {
+        if (!flagonetime)
+        {
+            sleepTime += Time.deltaTime;
+
+            if (sleepTime > timeLimit) flagonetime = true;
+        }
+
+        if (flagonetime)
+        {
+
+            //transform.Rotate(Vector3.up * Time.deltaTime * 20, Space.World);
+            SceneNavigator.Instance.Change(SceneName, 0.5f);
+
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            sleepTime = 0;
+            flagonetime = false;
+        }
+    }
+}
+
+
+    //SceneNavigator.Instance.Change(scenename, 0.5f);
